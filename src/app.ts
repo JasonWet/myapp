abc6c0e196ee716bdac4cac5148d395f1562099d
jasonwet <jason@colonialpress.net>
2020-09-19 18:26:36 -0400

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+
+const add = (a: number, b: number): number => a + b;
+const say = (a: string, b: string): string => a + ' ' + b;
+
+app.get('/', (req : Request, res : Response, next: NextFunction) => {
+    console.log(add(1, 1))
+    res.send(say('Jason', 'Dry'));
+});
+
+app.listen(8080, () => console.log('Server Running!'))
