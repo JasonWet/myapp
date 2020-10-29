74b072fcca817a5d3db5321a5bb835927530ff9b
jasonwet <jason@colonialpress.net>
2020-10-29 16:11:48 -0400

    say hello

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,10 @@ const add = (a: number, b: number): number => a + b;
 const say = (a: string, b: string): string => a + ' ' + b;
 
 app.get('/', (req : Request, res : Response, next: NextFunction) => {
-    console.log(add(1, 1))
-    res.send(say('Jason', 'Dry'));
+    console.log(add(3, 1))
+    res.send('Hello There')
+    // res.send(add(5, 10))
+    // res.send(say('Jason', 'Dry'));
 });
 
 app.listen(8080, () => console.log('Server Running!'))
